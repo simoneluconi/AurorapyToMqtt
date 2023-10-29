9fa440cdedfcf131e58de70cfced6045371b833f
serotonie <68992449+serotonie@users.noreply.github.com>
2023-10-29 21:11:41 +0100

    Move Advertise

--- app/AuroraComm.py
+++ app/AuroraComm.py
@@ -13,7 +13,6 @@ from HassAutodiscovery import Advertise
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):    
     print("AuroraMQTT Connected with result code "+str(rc))
-    Advertise(client, PowerOne(0), os.getenv('MQTT_TOPIC'))
     client.publish(os.getenv('MQTT_TOPIC')+"/status",payload="online", qos=0, retain=True)
 
 # The callback for when a PUBLISH message is received from the server.
@@ -29,7 +28,6 @@ client.on_message = on_message
 client.username_pw_set(os.getenv('MQTT_USERNAME'), os.getenv('MQTT_PASSWORD'))
 client.will_set(os.getenv('MQTT_TOPIC')+"/status", payload="offline", qos=0, retain=True)
 client.connect(os.getenv('MQTT_BROKER_HOST'), int(os.getenv('MQTT_BROKER_PORT')), 60)
-time.sleep(5)
 client.loop_start()
 
 sunup = 0
@@ -41,18 +39,30 @@ while True:
             print('Sun is up, starting polling every two seconds\n')
             client.loop_start()
             sunup = sunup + 1           
+        
+        PowerOne = PowerOne(1)
 
-        jsonRes = json.dumps(PowerOne(1))
+        Advertise(client, PowerOne, os.getenv('MQTT_TOPIC'))
+
+        jsonRes = json.dumps(PowerOne)
         client.publish(os.getenv('MQTT_TOPIC'), jsonRes)
 
         time.sleep(2)
     
     else:
         print('Sun is down, stopping polling until tomorrow\n')
-        jsonRes = json.dumps(PowerOne(0))
+
+        PowerOne = PowerOne(0)
+
+        Advertise(client, PowerOne, os.getenv('MQTT_TOPIC'))
+
+        jsonRes = json.dumps(PowerOne)
         client.publish(os.getenv('MQTT_TOPIC'), jsonRes, retain=True)
+
         client.loop_stop()
+
         wait(lambda: IsSunUp(), sleep_seconds=300)
+        
         sunup = 0
 
     
